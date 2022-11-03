8ea94c547594164dc37da976fe402ad9edc38c8f
eugene.telenchenko <etelenchenko@gmail.com>
2022-11-03 19:25:55 +0300

    added api gateway service

--- products-service/src/main/java/com/etelenchenko/productsservice/controller/ProductController.java
+++ products-service/src/main/java/com/etelenchenko/productsservice/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.etelenchenko.productsservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public String index() {
+        return "Products service is running";
+    }
 
 }
