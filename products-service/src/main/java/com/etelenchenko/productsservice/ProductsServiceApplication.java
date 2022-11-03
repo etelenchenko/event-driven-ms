55b1a52641ac8e1941470f869152ffb53a479007
eugene.telenchenko <etelenchenko@gmail.com>
2022-11-03 19:07:52 +0300

    created discovery service project and adjusted products service to register itself in eureka discovery

--- products-service/src/main/java/com/etelenchenko/productsservice/ProductsServiceApplication.java
+++ products-service/src/main/java/com/etelenchenko/productsservice/ProductsServiceApplication.java
@@ -2,7 +2,9 @@ package com.etelenchenko.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductsServiceApplication {
 
