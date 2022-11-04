83c44b1284c61d2b32950532ab51594f2ca216c5
eugene.telenchenko <etelenchenko@gmail.com>
2022-11-04 17:39:25 +0300

    added axon server docker config, added properties to start multiple instances of products service

--- products-service/src/main/java/com/etelenchenko/productsservice/controller/ProductController.java
+++ products-service/src/main/java/com/etelenchenko/productsservice/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.etelenchenko.productsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping
     public String index() {
-        return "Products service is running";
+        return "Products service is running, port: " + environment.getProperty("local.server.port");
     }
 
 }
